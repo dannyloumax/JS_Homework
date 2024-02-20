8928e4b2edb0d0885486f255552681da8910a0c4
Maksim Danilau <dannyloumax@gmail.com>
2024-02-20 23:20:36 +0300

    init(hw-17): created new branch and added task 1

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Task 1 ----------------------------------
+
+let a = 'true';
+let b = false;
+let c = 17;
+let d = undefined;
+let e = null;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+
+

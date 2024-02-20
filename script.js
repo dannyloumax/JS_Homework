750045ea4737f6fee2df2bed3c2791beb3c9fb89
Maksim Danilau <dannyloumax@gmail.com>
2024-02-20 23:49:28 +0300

    refactor(hw-17): improved a condition for task 3

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ if (height > weight) {
 // Task 3 ----------------------------------
 
 for (let i = 1; i <= 20; i++) {
-    if (i % 3 == 0) {
+    if (i % 3 === 0) {
         console.log(i);
     }
 }

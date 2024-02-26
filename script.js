5e09872a785c89d2fc1adf09b543813a0a6c3d64
Maksim Danilau <dannyloumax@gmail.com>
2024-02-26 20:24:30 +0300

    feat(hw-18): added task 2

--- script.js
+++ script.js
@@ -10,5 +10,21 @@ function getSum(num) {
     console.log(sum);
 }
 
-let number = prompt('Пожалуйста, введите число: ');
+let number = prompt('Пожалуйста, введите число: ', 0);
 getSum(number);
+
+// Task 2 -----------------------------------
+
+function creditRateCalc (cost) {
+    let years = 5;
+    let interestOnCredit = 0;
+
+    for (let i = 0; i < years; i++) {
+        interestOnCredit += (cost/100)*17;
+    }
+    console.log(interestOnCredit);
+}
+
+let creditSum = prompt('Пожалуйста, введите сумму:', 0);
+creditRateCalc(creditSum);
+

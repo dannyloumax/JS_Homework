f0c5ee4e3a910dc7b35e91e1a07834629419b9ef
Maksim Danilau <dannyloumax@gmail.com>
2024-02-26 17:29:51 +0300

    feat(hw-18): added task 1

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// Task 1 ----------------------------------
+
+function getSum(num) {
+    let sum = 0;
+    for (let i = 0; i <= num; i++) {
+        sum += i;
+    }
+    console.log(sum);
+}
+
+let number = prompt('Пожалуйста, введите число: ');
+getSum(number);

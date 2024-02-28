15799fdf15424324170e7b2c4c5f46d6ce338489
Maksim Danilau <dannyloumax@gmail.com>
2024-02-28 21:35:58 +0300

    feat(hw-18): added task 4

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ console.log(calcSum(number));
 
 // Task 2 -----------------------------------
 
-function calcCreditRate (cost) {
+function calcCreditRate(cost) {
     let years = 5;
     let interestOnCredit = 0;
 
@@ -29,3 +29,22 @@ let creditSum = prompt('Пожалуйста, введите сумму:', 0);
 console.log(calcCreditRate(creditSum));
 
 
+// Task 3 ----------------------------------
+
+function trimString(str, from, to) {
+
+}
+
+// Task 4 ----------------------------------
+
+function getSumNumber(num) {
+    let sum = 0;
+
+    for (let i = 0; i < num.length; i++) {
+        sum += Number(num[i]);
+    }
+    return sum;
+}
+
+let numberTask3 = prompt('Введите номер:', 0);
+console.log('Сумма чисел равняется', getSumNumber(numberTask3));

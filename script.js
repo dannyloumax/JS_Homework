b7582d3fd2c8bf50cdcb8293b4dc3478779c21da
Maksim Danilau <dannyloumax@gmail.com>
2024-03-02 13:07:25 +0300

    feat(hw-19): added task 2

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ const newObject = {
 delete newObject.id;
 delete newObject.name;
 console.log(newObject);
+
+// Task 2 --------------------------------
+
+const objectTask2 = {
+    id: 2,
+    name: 'Vasya'
+}
+let key = prompt('Введите свойство: ')
+key in objectTask2 ? console.log(true) : console.log('Такого ключа нет!');
+
+/*
+console.log(key in objectTask2 ? true : 'Такого ключа нет!');
+OR
+let isExists = key in objectTask2 ? true : 'Такого ключа нет!';
+console.log(isExists);
+ */

53f26586c931082a09a28846d18dd1d8f7b14946
Maksim Danilau <dannyloumax@gmail.com>
2024-03-02 13:14:29 +0300

    feat(hw-19): added task 3

--- script.js
+++ script.js
@@ -23,3 +23,19 @@ OR
 let isExists = key in objectTask2 ? true : 'Такого ключа нет!';
 console.log(isExists);
  */
+
+// Task 3 --------------------------------
+
+const student = {
+    name: 'John',
+    age: 19,
+    isHappy: true
+}
+
+for (let key in student) {
+    console.log(key);
+}
+
+for (let key in student) {
+    console.log(student[key]);
+}

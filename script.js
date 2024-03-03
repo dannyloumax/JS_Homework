b4eee0cc8e6864695dbf7cc85a638bd2e60af8d2
Maksim Danilau <dannyloumax@gmail.com>
2024-03-03 23:42:45 +0300

    feat(hw-20): added task 4

--- script.js
+++ script.js
@@ -9,3 +9,13 @@
 
  const animals = ['monkey', 'dog', 'cat'];
  console.log(animals[animals.length-1]);
+
+ // Task 4 -----------------------------------
+
+ const students = ['Polina', 'Dasha', 'Masha'];
+
+ students.pop();
+ students.push('Borya');
+ students.shift();
+ students.unshift('Andrey');
+ console.log(students);

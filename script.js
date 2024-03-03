43658fd430e6a76b98491999e5bd58f11807004f
Maksim Danilau <dannyloumax@gmail.com>
2024-03-03 23:18:48 +0300

    feat(hw-20): added task 2

--- script.js
+++ script.js
@@ -4,3 +4,8 @@
 
  const colors = ['red', 'green', 'blue'];
  console.log(colors.length);
+
+ // Task 2 -----------------------------------
+
+ const animals = ['monkey', 'dog', 'cat'];
+ console.log(animals[animals.length-1]);

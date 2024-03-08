bfea03fdf77c3ff6136f39a6a00103bdc24112da
Maksim Danilau <dannyloumax@gmail.com>
2024-03-09 00:01:31 +0300

    feat(hw-21): added task 1

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 "use strict";
 
+// Task 1 -------------------------------
 
+const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987];
+
+// The first option
+fibonacci.forEach(function (number) {
+    console.log(number);
+})
+
+// The second option
+fibonacci.forEach(number => console.log(number));
